546e99790af70c80db7c9d4f5a7ef6fc23af3095
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-10-28 09:48:46 -0400

    created my game loop

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,8 +5,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+	Scanner keyboard;
+	keyboard = new Scanner(System.in);
 
+	int randomNumber, userNumber;
+	randomNumber = (int) (Math.random() * 100);
+	System.out.println(randomNumber);
+
+	System.out.println("I am thinking of a 1-100 guess what it is");
+	userNumber = keyboard.nextInt();
+
+	if (userNumber == randomNumber){
+		System.out.println(randomNumber+ " is the correct answer");
 	}
-	public static String numberguessinggame() {
-		
+while (userNumber < randomNumber || userNumber > randomNumber){
+
+		if (userNumber < randomNumber){
+			System.out.print("You guessed to low try again");
+			userNumber = keyboard.nextInt();
+		}
+		if (userNumber > randomNumber){
+			System.out.println("You gussed to high try again");
+			userNumber = keyboard.nextInt();
+		}
+		if (userNumber == randomNumber){
+			System.out.println(randomNumber+ " is the correct number you got it right");
+		}
+
+
+}
+    }
 }
